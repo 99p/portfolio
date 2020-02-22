a5b51bc64f2018e7832a18061341650ead27b064
99p <poccori@gmail.com>
2020-02-22 14:26:14 +0900

    yeah!

--- config/views.py
+++ config/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.views.generic import TemplateView
 
 class index(TemplateView):
-    template_name = 'startbootstrap-grayscale/index.html'
+    template_name = 'index.html'
 
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
