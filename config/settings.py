06d23b492d2f5fe08f91c1acf60feb8596e43932
99p <poccori@gmail.com>
2020-02-22 12:24:36 +0900

    add bootstrap4

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
+import mimetypes
+
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -36,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
--- config/views.py
+++ config/views.py
@@ -3,4 +3,9 @@ from django.http import HttpResponse
 from django.views.generic import TemplateView
 
 class index(TemplateView):
-    template_name = 'index.html'
+    template_name = 'startbootstrap-grayscale/index.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['title'] = "ponponboy's Portfolio"
+        return ctx
